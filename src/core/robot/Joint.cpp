453b7915f80bc542a1a0ef62bc260537b9a5558f
TomMinuzzo <tom.minuzzo@gmail.com>
2022-02-02 18:44:54 +1100

    SendAnkleCmd test

--- src/core/robot/Joint.cpp
+++ src/core/robot/Joint.cpp
@@ -122,6 +122,8 @@ setMovementReturnCode_t Joint::setPosition(double desQ) {
             if (driveMode == CM_POSITION_CONTROL) {
                 if(id == 4 || id == 5){
                     std::cout << "[Joint:setPosition] Actuate Ankle " << id << " to " << jointPositionToDriveUnit(desQ + q0) << std::endl;
+                    drive->setPos(jointPositionToDriveUnit(desQ + q0));
+                    drive->posControlConfirmSP();
                 }
                 else{
                     drive->setPos(jointPositionToDriveUnit(desQ + q0));
