67705b2884fe0a4ed364feacc226e4de5d652cf6
TomMinuzzo <tom.minuzzo@gmail.com>
2022-02-02 18:58:42 +1100

    missing bracket

--- src/core/robot/Joint.cpp
+++ src/core/robot/Joint.cpp
@@ -122,7 +122,7 @@ setMovementReturnCode_t Joint::setPosition(double desQ) {
             if (driveMode == CM_POSITION_CONTROL) {
                 if(id == 4 || id == 5){
                     std::cout << "[Joint:setPosition] Actuate Ankle " << id << " to " << jointPositionToDriveUnit(desQ + q0) << std::endl;
-                    drive->setPos(0));
+                    drive->setPos(0);
                     drive->posControlConfirmSP();
                 }
                 else{
