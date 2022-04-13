8532c47440d07b8ffaef5cb5388aca8c60a1ae90
TomMinuzzo <tom.minuzzo@gmail.com>
2022-04-13 12:39:38 +1000

    Fixed missing output syntax

--- src/apps/AlexMachine/TrajectoryGenerator/AlexTrajectoryGenerator.cpp
+++ src/apps/AlexMachine/TrajectoryGenerator/AlexTrajectoryGenerator.cpp
@@ -1232,8 +1232,8 @@ std::vector<taskspace_state> AlexTrajectoryGenerator::generate_key_taskspace_sta
             keyTaskspaceStates.push_back(stateEnd);
         }
         
-    }
-    return keyTaskspaceStates;*/
+    }*/
+    return keyTaskspaceStates;
 }
 
 // Generates discrete trajectory from parameters to use in control system
